d44cf73eb07de11f1b7415e868d46b18c2fd5c11
Joshua Harris <joshua40harris@gmail.com>
2015-04-09 13:55:22 -0400

    fixed setup script

--- setup_mac_config.sh
+++ setup_mac_config.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-## ask if user wants to keep their current .files
-read -p "Do you want to keep your current .files (y/n)? " -n 1 -r
-echo
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo "yanking files from ~"
-	./yank_mac_config.sh
-fi
-
 ## Update the zsh files
-cp ./.zshrc ~/.zshrc
+cp zsh/.zshrc ~/.zshrc
 
 ## Update the vim files
 cp vim/.vimrc ~/.vimrc
-cp -r vim/.vimtemplates ~/.vimtemplates
+cp -r vim/.vimtemplates ~/
 
 ## Update the tmux files
 cp tmux/.tmux.conf ~/.tmux.conf
