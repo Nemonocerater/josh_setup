6aa57606ae608617e645c561e706d35f1eb984d8
Joshua Harris <joshua40harris@gmail.com>
2015-04-09 18:22:48 -0400

    removed comments

--- setup_mac_config.sh
+++ setup_mac_config.sh
@@ -3,18 +3,14 @@
 ## Update the zsh files
 rm ~/.zshrc 
 ln -s $HOME/code/josh_setup/zsh/.zshrc ~/.zshrc
-#cp zsh/.zshrc ~/.zshrc
 
 ## Update the vim files
 rm ~/.vimrc
 ln -s $HOME/code/josh_setup/vim/.vimrc ~/.vimrc
-#cp vim/.vimrc ~/.vimrc
 
 rm -rf ~/.vimtemplates
 ln -s $HOME/code/josh_setup/vim/.vimtemplates ~/.vimtemplates
-#cp -r vim/.vimtemplates ~/
 
 ## Update the tmux files
 rm ~/.tmux.conf
 ln -s $HOME/code/josh_setup/tmux/.tmux.conf ~/.tmux.conf
-#cp tmux/.tmux.conf ~/.tmux.conf
